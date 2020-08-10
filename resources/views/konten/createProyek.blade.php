52d5c2310c4c50a876fc982c6d5a59536b91aacc
aymrei14 <ghinak@student.telkomuniversity.ac.id>
2020-08-10 22:12:34 +0700

    kuis 3 revisi

--- resources/views/konten/createProyek.blade.php
+++ resources/views/konten/createProyek.blade.php
@@ -1,5 +1,57 @@
 @extends('layouts.master')
 
 @section('content')
-
+<div class="card card-primary">
+    <div class="card-header">
+        <h3 class="card-title">Buat Proyek</h3>
+    </div>
+    <!-- /.card-header -->
+    <!-- form start -->
+    <form role="form" action="/proyek" method="POST">
+        @csrf
+        <div class="card-body">
+            <div class="form-group">
+                <label for="title">Nama Proyek</label>
+                @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <input type="text" class="form-control" id="title" placeholder="Enter title" name="title" value="{{ old('title', '') }}">
+            </div>
+            <div class="form-group">
+                <label for="desc">Deskripsi</label>
+                @error('desc')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <textarea class="form-control" id="desc" rows="3" placeholder="Your description here..." name="desc">{{ old('desc', '') }}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="mulai">Mulai Proyek</label>
+                @error('mulai')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <input type="date" class="form-control" id="mulai"  name="mulai" value="{{ old('mulai', '') }}">
+            </div>
+            <div class="form-group">
+                <label for="dl">Deadline Proyek</label>
+                @error('dl')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <input type="date" class="form-control" id="dl"  name="dl" value="{{ old('dl', '') }}">
+            </div>
+            <div class="form-group">
+                <label for="manager">Manager Proyek</label>
+                @error('manager')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <select class="custom-select" id="inputGroupSelect01">
+                    <option value=""></option>
+                </select>
+            </div>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</div>
 @endsection
